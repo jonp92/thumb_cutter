2adb039551f5bedd49e2d3c396cd7e5e4da3c9c4
jonathan <jonathanpressler@icloud.com>
2023-02-18 13:36:02 -0500

    added SCP transfer along with variable in the config.ini file.

--- thumb_cutter_v2.py
+++ thumb_cutter_v2.py
@@ -3,6 +3,7 @@ import base64
 import re
 import time
 import configparser
+import subprocess
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -33,16 +34,24 @@ class FileHandler(FileSystemEventHandler):
                         f.write(thumbnail_data)
                     print(f'Saved thumbnail as {output_path}')
 
+                    # Copy the file to the remote server using scp
+
+                    scp_command = ['scp', '-P', port, output_path, f'{username}@{server}:{remote_dir}']
+                    subprocess.run(scp_command)
+
 
 if __name__ == '__main__':
     # Read the configuration file
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    # Get the directory paths from the configuration file
+    # Get the variables from the configuration file
     input_dir = config.get('directories', 'input_dir')
     output_dir = config.get('directories', 'output_dir')
-
+    username = config.get('remote', 'username')
+    server = config.get('remote', 'server')
+    remote_dir = config.get('remote', 'remote_dir')
+    port = config.get('remote', 'port')
     # Create the FileHandler and Observer objects
     event_handler = FileHandler()
     observer = Observer()
